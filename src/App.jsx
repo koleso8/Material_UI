798a773f0e2c50d8dbc00491b8ff236084538750
koleso8 <dev.koleso@gmail.com>
2024-09-28 10:32:16 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {
   Button,
   Container,
   IconButton,
+  Skeleton,
   Toolbar,
   Typography,
 } from '@mui/material';
@@ -12,29 +13,37 @@ import BasicPagination from './components/BasicPagination';
 
 const App = () => {
   return (
-    <AppBar position="fixed">
-      <Container fixed>
-        <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography mr={'auto'} variant="h6">
-            Material UI
-          </Typography>
-          <Box mr={4}>
-            <Button color="inherit" variant="outlined">
-              Log In
-            </Button>
-          </Box>
-          <Box>
-            <Button color="secondary" variant="contained">
-              Sign Up
-            </Button>
-          </Box>
-        </Toolbar>
-      </Container>
-      <BasicPagination />
-    </AppBar>
+    <div>
+      <AppBar position="fixed">
+        <Container>
+          <Toolbar>
+            <IconButton edge="start" color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography mr={'auto'} variant="h6">
+              Material UI
+            </Typography>
+            <Box mr={4}>
+              <Button color="inherit" variant="outlined">
+                Log In
+              </Button>
+            </Box>
+            <Box>
+              <Button color="secondary" variant="contained">
+                Sign Up
+              </Button>
+            </Box>
+          </Toolbar>
+        </Container>
+        {/* <BasicPagination /> */}
+      </AppBar>
+      {(
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSri6L0rymvKYkambxYKvPYzIcLtFlsZxYzUw&s"
+          alt=""
+        />
+      ) && <Skeleton variant="rectangular" width={210} height={218} />}
+    </div>
   );
 };
 
